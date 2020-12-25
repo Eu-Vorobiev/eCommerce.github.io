678ddbd079d157808cd81a2e7f5682e8ac26dd4c
Eugene <dijjekas06@gmail.com>
2020-12-25 12:48:36 +0200

    Full homepage / slider doesnt work

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
-$(".category__item--nav").on(click, function (){
-  $(".category__sublist--nav").addClass(visible)
+// $(".category__item--nav").on(click, function (){
+//   $(".category__sublist--nav").addClass(visible)
+// });
+$(function() {
+
+  $(".reviews__slider").slick({
+    infinite: true,
+    fade: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true
+  });
+
 });
